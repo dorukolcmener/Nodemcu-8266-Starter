57cfa1731dc66bcf05f983f1e69768f8e5a392d2
dorukolcmener <facebondgata@outlook.com>
2023-03-05 12:04:42 +0000

    Release v1.01

--- README.md
+++ README.md
@@ -1,8 +1,49 @@
 # Nodemcu-8266-Starter
 
 ## Description
+
 This is my personal starter template to get into fresh 8266 module.
 Firmware binary is acquired from http://nodemcu-build.com/, use it with your own risk.
 
+## Usage
+
+1- Prerequisites
+
+- [NodeMCU ESP8266](https://en.wikipedia.org/wiki/NodeMCU)
+- [Esptool.py](https://docs.espressif.com/projects/esptool/en/latest/esp8266/index.html)
+- [NodeJS](https://nodejs.org/en/download/package-manager/#debian-and-ubuntu-based-linux-distributions)
+- Firmware (Optional)
+
+2- Clone
+
+```
+git clone https://github.com/dorukolcmener/Nodemcu-8266-Starter.git
+```
+
+3- Open with your favourite IDE
+
+```
+code Nodemcu-8266-Starter
+```
+
+4- Intall npm package
+
+```
+npm install
+```
+
+5- Connect the board to your computer via USB port and intall the firmware to the board
+
+```
+npm run flash-fw
+```
+
+4- Compile and run the code on the IoT card.
+
+```
+npm run uc
+```
+
 ## License
+
 [MIT](LICENSE)
--- app/init.lua
+++ app/init.lua
@@ -1,26 +1,61 @@
+-- ******************************************                            Initialize GPIO
 pin = 0
-pin2 = 8
-
 gpio.mode(pin,gpio.OUTPUT)
-gpio.mode(pin2,gpio.OUTPUT)
+gpio.write(pin,1)
 
-tobj = tmr.create()
-tobj2 = tmr.create()
-
-tobj:alarm(1000,tmr.ALARM_AUTO,function() 
-    -- print("alarm 1")
-    if gpio.read(pin) == 0 then
-        gpio.write(pin,1)
-    else
+-- ******************************************                            Helper Functions
+function toggleLed()
+    if (gpio.read(pin) == 1) then
         gpio.write(pin,0)
+    else
+        gpio.write(pin,1)
     end
+end
+
+function sendResponse(res, code, body)
+    res:send(nil, code)
+    res:send_header("Connection", "close")
+    res:send("<html><head><title>ESP8266</title></head><body><h1>ESP8266</h1>")
+    res:send(body)
+    res:send("</body></html>")
+    res:finish()
+end
+
+-- ******************************************                            Connect to wifi
+-- Connect to wifi named Rivendell
+wifi.setmode(wifi.STATION)
+-- Sta wifi config
+station_cfg={}
+station_cfg.ssid="Secret Wifi"
+station_cfg.pwd="Secret Password"
+station_cfg.save=false
+station_cfg.auto=true
+wifi.sta.config(station_cfg)
+-- Set hostname
+if (wifi.sta.sethostname("esp8266") == true) then
+    print("hostname was successfully changed")
+else
+    print("hostname was not changed")
+end
+-- Light led on connect
+wifi.eventmon.register(wifi.eventmon.STA_CONNECTED, function(T)
+    gpio.write(pin,0)
 end)
 
-tobj2:alarm(200,tmr.ALARM_AUTO,function() 
-    -- print("alarm 2")
-    if gpio.read(pin2) == 0 then
-        gpio.write(pin2,1)
+-- ******************************************                            Create httpserver
+require("httpserver").createServer(80, function(req, res)
+    -- If request url is / then send index.html
+    if req.url == "/" then
+        body = "<p>Hostname: " .. wifi.sta.gethostname() .. "</p><p>IP: " .. wifi.sta.getip() .. "</p>"
+        sendResponse(res, 200, body)
+    
+    -- If request url is /toggle then toggle led
+    elseif req.url == "/toggle" then
+        toggleLed()
+        sendResponse(res, 200, "<p>Toggled led.</p>")
+    
+    -- Send 404
     else
-        gpio.write(pin2,0)
+        sendResponse(res, 404, "<p>404 Not Found</p>")
     end
 end)
